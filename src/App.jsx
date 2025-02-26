9def3ebf70e3a8d186126ce9227a3b347f818225
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-26 17:02:52 +0100

    init project: install react, axios, react router and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
